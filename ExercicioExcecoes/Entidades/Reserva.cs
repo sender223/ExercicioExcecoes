3f58668ac67640a9d204364abf5b4304e7d6e53f
Sender Rodrigues dos Santos <sender.rsantos@gmail.com>
2019-10-10 13:19:13 -0300

    Primiera parte, mostrar a solução muito ruim para o problema

---
+++ ExercicioExcecoes/Entidades/Reserva.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace ExercicioExcecoes.Entidades {
+    class Reserva {
+        public int NumeroQuarto { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int numeroQuarto, DateTime checkIn, DateTime checkOut) {
+            NumeroQuarto = numeroQuarto;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+        //metodo de duração das diarias, 
+        public int Duracao() {
+            //diferença entre os dois é a duração da diaria
+            TimeSpan duracao = CheckOut.Subtract(CheckIn);
+            //como o TotalDays é um double, precisamos fazer um casting 
+            //para converter ele em inteiro e assim saber a quantidade
+            //de dias que ficou hospedado.
+            return (int)duracao.TotalDays;
+        }
+
+        public void AtualizarDatas(DateTime checkIn, DateTime checkOut) {
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString() {
+            return "Quarto: "
+                + NumeroQuarto
+                + ", Check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", Check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duracao()
+                + " Noites";
+        }
+
+
+
+    }
+}
--- ExercicioExcecoes/Program.cs
+++ ExercicioExcecoes/Program.cs
@@ -1,9 +1,43 @@
 ﻿using System;
+using ExercicioExcecoes.Entidades;
 
 namespace ExercicioExcecoes {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.Write("Numero do Quarto: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Data de Check-in (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Data de Check-out (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            if (checkOut <= checkIn) {
+                Console.WriteLine("Erro na reserva: Data de Check-out deve ser posterior a Data de Check-In");
+            }
+            else {
+                Reserva reserva = new Reserva(numero, checkIn, checkOut);
+                Console.WriteLine("Reserva: " + reserva);
+
+                Console.WriteLine();
+                Console.WriteLine("Entre com os dados para atualizar a reserva: ");
+                Console.Write("Data de Check-in (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Data de Check-out (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+
+                if (checkIn < now || checkOut < now) {
+                    Console.WriteLine("Erro: As datas da reserva para atualizar devem ser futuras: ");
+                }
+                else if (checkOut <= checkIn) {
+                    Console.WriteLine("Erro na reserva: Data de Check-out deve ser posterior a Data de Check-In");
+                }
+                else {
+                    reserva.AtualizarDatas(checkIn, checkOut);
+                    Console.WriteLine("Reserva: " + reserva);
+                }
+            }
         }
     }
 }
